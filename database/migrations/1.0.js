ecc1dac837fef32c88da74d35b37a1989d88e5df
AxonDragonScale <r.r.n.harkhani@gmail.com>
2019-01-21 01:51:16 +0530

    Set up the migration files (1.0.js).
    1.0.js has migration functionality for creating and dropping database
    tables.

---
+++ database/migrations/1.0.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const Bluebird = require("bluebird");
+const sqlite = require("sqlite3");
+const path = require("path");
+
+// can be imported in other files by require()
+module.exports = {
+    // up makes tables and down destroys them
+    up: function() {
+        return new Bluebird (function(resolve, reject) {
+            // migration function
+            let db = new sqlite.Database('./database/InvoiceApp.db');
+            db.run(`PRAGMA foreign_keys = ON`);     // enable foreign key constraints
+
+            // serialize specifies that the queries run sequentially not simultaneously
+            db.serialize(function() {
+                db.run(`CREATE TABLE users (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    email TEXT,
+                    company_name TEXT,
+                    password TEXT
+                )`);
+
+                db.run(`CREATE TABLE invoices (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    user_id TEXT,
+                    paid NUMERIC,
+                    FOREIGN KEY(user_id) REFERENCES users(id)
+                )`);
+
+                db.run(`CREATE TABLE transactions (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    price INTEGER,
+                    FOREIGN KEY(invoice_id) REFERENCES invoices(id)
+                )`);
+
+            });
+
+            db.close();
+        });
+    },
+
+    down: function() {
+        return new Bluebird(function(resolve, reject) {
+            // runs if we want to rollback changes.
+            // reverts the up function and brings database back to initial state
+            let db = new sqlite.Database('./database/InvoiceApp.db');
+            db.serialize(function() {
+                db.run(`DROP TABLE transactions`);
+                db.run(`DROP TABLE invoices`);
+                db.run(`DROP TABLE users`);
+            });
+
+            db.close();
+        });
+    }
+
+};
