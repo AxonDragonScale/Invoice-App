0cf0c4ae6b5e7d1c8b753bf203c88d36f80b35f4
AxonDragonScale <r.r.n.harkhani@gmail.com>
2019-01-21 17:24:07 +0530

    Bug in /register fixed. /login route set-up.
    register and login now works.

--- server.js
+++ server.js
@@ -25,19 +25,19 @@ app.listen(PORT, function () {
 
 
 
-// setting up /register route
+// setting up /register route using POST
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-// function isEmpty(str) {
-//     return !str || 0 === str.length;
-// }
+function isEmpty(str) {
+    return !str || 0 === str.length;
+}
 
 // post() routes HTTP post request to specified path and calls the function
 app.post('/register', function (req, res) {
-    // if any field is empty, it fails
-    if (req.body.name === '' || req.body.email === '' ||
-        req.body.company_name === '' || req.body.password === '') {
+    //if any field is empty, it fails
+    if (isEmpty(req.body.name) || isEmpty(req.body.email) ||
+        isEmpty(req.body.company_name) || isEmpty(req.body.password)) {
         return res.json({
             'status': false,
             'message': 'All fields required'
@@ -47,8 +47,7 @@ app.post('/register', function (req, res) {
     // if all fields are properly present, save user in database
     bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
         let db = new sqlite.Database("./database/InvoiceApp.db");
-        let sql = `INSERT INTO users(name, email, company_name, password) 
-                   VALUES('${req.body.name}','${req.body.email}','${req.body.company_name}','${hash}')`;
+        let sql = `INSERT INTO users(name, email, company_name, password) VALUES('${req.body.name}','${req.body.email}','${req.body.company_name}','${hash}')`;
 
         db.run(sql, function (err) {
             if (err) {
@@ -56,11 +55,51 @@ app.post('/register', function (req, res) {
             } else {
                 return res.json({
                     status: true,
-                    'message': 'User Created'
+                    message: 'User Created'
                 });
             }
         });
 
         db.close();
+    });
+});
+
+
+
+// setting up the /login route using POST
+app.post('/login', function (req, res) {
+    let db = new sqlite.Database('./database/InvoiceApp.db');
+    let sql = `SELECT * FROM users WHERE email='${req.body.email}'`;
+
+    // all() runs the sql commands and returns the result in rows array
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        db.close();
+
+        // if result array rows is empty, no user exists
+        if (rows.length == 0) {
+            return res.json({
+                status: false,
+                message: "Sorry, Incorrect email"
+            });
+        }
+
+        let user = rows[0];
+        let authenticated = bcrypt.compareSync(req.body.password, user.password);   // true if password matches
+        delete user.password;   // delete the password entered by user for security
+
+        if (authenticated) {
+            return res.json({
+                status: true,
+                user: user
+            });
+        } else {
+            return res.json({
+                status: false,
+                message: "Password is Incorrect"
+            });
+        }
     });
 });
